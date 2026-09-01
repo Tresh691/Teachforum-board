63fcd33d37d0c19ff0de8bb24383be45a4d207b2
Tresh691 <tytupoi27@gmail.com>
2026-09-01 23:29:03 +0300

    Добавлена серверная часть node.js + express + PostgreSQL, написаны первые эндпоинты для доски

---
+++ server/src/index.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Доска работает!')
+})
+
+app.post('/boards', async (req, res) => {
+const { title, data } = req.body
+if (!title){
+  return res.status(400).json({message: 'Поле не должно быть пустым'})
+}
+try{
+  const newBoard = await prisma.board.create({
+    data: {
+      title: title,
+      data: data || {}
+    }
+  })
+  res.status(201).json(newBoard)
+} catch(error) {
+  res.status(500).send('Не удалось добавить доску')
+  console.error(error)
+}
+})
+
+app.get('/boards/:id', async (req,res) => {
+  const id = req.params.id
+  try{
+    const board = await prisma.board.findUnique({where: { id } })
+    if (board == null){
+      return res.status(404).json({message: 'Доска не найдена'})
+    }
+    res.status(200).json(board)
+  } catch (error){
+    res.status(500).send('Не удалось открыть доску')
+    console.error(error)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
+})
